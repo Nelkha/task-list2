91c57e5183a94fef8f8a298aa558a5aaa6ced6ec
Nelkha <maritogallego@hotmail.com>
2022-07-23 23:37:17 -0300

    Lista de Tareas con icono

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { TASKS } from 'src/app/mock-task';
 import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -8,6 +9,7 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent implements OnInit {
 @Input() task:Task = TASKS[0]
+faTimes=faTimes;
   constructor() { }
 
   ngOnInit(): void {
